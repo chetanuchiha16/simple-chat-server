62f5289f2abb462c21c6bf367d35732630edff31
chetanuchiha16 <chetan16ck@gmail.com>
2026-05-08 20:28:48 +0530

    try to use channel of slice and fail

---
+++ trial2/main.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"net"
+)
+
+func main() {
+
+	listener, err := net.Listen("tcp", ":8080")
+	if err != nil {
+		fmt.Println(err)
+	}
+	// no_of_clients := 0
+	clients := make(chan []net.Conn)
+
+	for {
+		conn, err := listener.Accept()
+		// clients = append(clients, conn)
+		clients <- append(<-clients, conn)
+		if err != nil {
+			println(err)
+		}
+		// no_of_clients += 1
+
+		go handleClient(conn, clients)
+
+	}
+
+}
+
+func handleClient(conn net.Conn, clients chan []net.Conn) {
+
+	fmt.Printf("connected %v\n", len(clients))
+	scanner := bufio.NewReader(conn)
+	for {
+
+		message, err := scanner.ReadString('\n')
+		if err != nil {
+			println(err)
+		}
+		fmt.Printf("recieved message: %v\n", message)
+		// conn.Write([]byte(fmt.Sprintf("sent message: %v", message)))
+		for i, client := range <-clients {
+			client.Write([]byte(fmt.Sprintf("recieved message %v from another client %d\n", message, i)))
+		}
+	}
+
+}
