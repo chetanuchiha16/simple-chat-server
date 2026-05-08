9b360e82fe571e3736103c7d464530a93eb9c114
chetanuchiha16 <chetan16ck@gmail.com>
2026-05-08 17:42:44 +0530

    use clients slice to broadcast messages

--- main/main.go
+++ main/main.go
@@ -4,40 +4,46 @@ import (
 	"bufio"
 	"fmt"
 	"net"
-	
-
 )
+
 func main() {
 
 	listener, err := net.Listen("tcp", ":8080")
 	if err != nil {
 		fmt.Println(err)
 	}
-	no_of_clients := 0
-	
+	// no_of_clients := 0
+	clients := make([]net.Conn, 0)
+
 	for {
 		conn, err := listener.Accept()
+		clients = append(clients, conn)
 		if err != nil {
 			println(err)
 		}
-		no_of_clients += 1
+		// no_of_clients += 1
 
-		go handleClient(conn, no_of_clients)
-		
+		go handleClient(conn, clients)
+
 	}
 
 }
 
-func handleClient(conn net.Conn, no_of_clients int) {
-	
-	fmt.Printf("connected %v\n", no_of_clients)
+func handleClient(conn net.Conn, clients []net.Conn) {
+
+	fmt.Printf("connected %v\n", len(clients))
 	scanner := bufio.NewReader(conn)
-	message, err := scanner.ReadString('\n')
-	if err != nil {
-		println(err)
+	for {
+
+		message, err := scanner.ReadString('\n')
+		if err != nil {
+			println(err)
+		}
+		fmt.Printf("recieved message: %v\n", message)
+		// conn.Write([]byte(fmt.Sprintf("sent message: %v", message)))
+		for i, client := range clients {
+			client.Write([]byte(fmt.Sprintf("recieved message %v from another client %d\n", message, i)))
+		}
 	}
-	fmt.Printf("recieved message: %v", message)
-	conn.Write([]byte(fmt.Sprintf("sent message: %v", message)))
-	conn.Close()
-	
+
 }
