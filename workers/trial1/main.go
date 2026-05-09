7ba3d4e4f955d3735b4dd8b5ec2c2a6e316e6002
chetanuchiha16 <chetan16ck@gmail.com>
2026-05-09 16:43:48 +0530

    refactor fanOut function to support WaitGroup for concurrent task synchronization

--- workers/trial1/main.go
+++ workers/trial1/main.go
@@ -1,25 +1,32 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"sync"
+)
 
-func fanOut(tasks <-chan int, workers int) []chan int {
+func fanOut(tasks <-chan int, workers int, wg *sync.WaitGroup) []chan int {
 	outs := make([]chan int, workers)
 	for i := range workers {
-		go func() {
+		wg.Add(1)
+		go func(id int) {
+			defer wg.Done()
 			for task := range tasks {
-				fmt.Printf("worker %v got task %v\n", i, task)
+				fmt.Printf("worker %v got task %v\n", id, task)
 			}
-		}()
+		}(i)
 	}
 	return outs
 }
 
 func main() {
 	tasks := make(chan int)
-	fanOut(tasks, 3)
+	var wg sync.WaitGroup
+	fanOut(tasks, 3, &wg)
 	for i := range 10 {
 		tasks <- i
 	}
 	close(tasks)
+	wg.Wait()
 
 }
